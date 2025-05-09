c95cbb09ad9c45151fe7b944e60dff6b1f207ec6
Emil <emmehdiyev@yahoo.com>
2025-05-09 17:02:33 +0400

    booking spring

---
+++ src/main/java/az/turing/bookingspring/BookingSpringApplication.java
@@ -0,0 +1,13 @@
+package az.turing.bookingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/turing/bookingspring/BookingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package az.turing.bookingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
